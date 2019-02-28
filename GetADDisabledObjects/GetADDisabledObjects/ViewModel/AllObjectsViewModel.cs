e73bbcb18c67a69a612dba3dd0a4e7aa494497ce
John Omelko <somethingTacos@hotmail.com>
2019-02-27 20:12:05 -0500

    General MVVM Setup

---
+++ GetADDisabledObjects/GetADDisabledObjects/ViewModel/AllObjectsViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetADDisabledObjects.ViewModel
+{
+    public class AllObjectsViewModel
+    {
+        private NavigationViewModel _navigationViewModel { get; set; }
+
+        public AllObjectsViewModel(NavigationViewModel navigationViewModel)
+        {
+            _navigationViewModel = navigationViewModel;
+
+        }
+    }
+}
